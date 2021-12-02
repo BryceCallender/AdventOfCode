3771fbbf0851a2ad101bdf45d069185e1c899352
Bryce Callender <brycec848@gmail.com>
2021-12-01 23:20:57 -0800

    Day 1 Advent of code!

--- AdventOfCode/Day_01.cs
+++ AdventOfCode/Day_01.cs
@@ -9,7 +9,44 @@ public class Day_01 : BaseDay
         _input = File.ReadAllText(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1() => Part1();
+    public override ValueTask<string> Solve_2() => Part2();
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    private ValueTask<string> Part1()
+    {
+        var increasing = 0;
+        var lines = _input.Split("\n").Select(int.Parse).ToArray();
+
+        for (var i = 1; i < lines.Length; i++)
+        {
+            if (lines[i] > lines[i - 1])
+            {
+                ++increasing;
+            }
+        }
+        
+        return new ValueTask<string>(increasing.ToString());
+    }
+
+    private ValueTask<string> Part2()
+    {
+        var increasing = 0;
+        var lines = _input.Split("\n").Select(int.Parse).ToArray();
+
+        for (var i = 0; i < lines.Length; i++)
+        {
+            if (i + 3 > lines.Length - 1)
+            {
+                break;
+            }
+
+            if (lines[i] < lines[i + 3])
+            {
+                ++increasing;
+            }
+                
+        }
+        
+        return new ValueTask<string>(increasing.ToString());
+    }
 }
