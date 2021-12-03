783402b52b0a08eb3c508451cb9bd755104d77b6
Bryce Callender <brycec848@gmail.com>
2021-12-02 20:44:21 -0800

    Day 2

---
+++ AdventOfCode/Day_02.cs
@@ -0,0 +1,78 @@
+namespace AdventOfCode;
+
+public class Day_02 : BaseDay
+{
+    private readonly string _input;
+
+    public Day_02()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1() => Part1();
+    public override ValueTask<string> Solve_2() => Part2();
+
+    private ValueTask<string> Part1()
+    {
+        var lines = _input.Split("\n").ToArray();
+
+        var position = 0;
+        var depth = 0;
+
+        foreach (var line in lines)
+        {
+            var splitCommand = line.Split(" ");
+
+            var command = splitCommand[0];
+            var value = int.Parse(splitCommand[1]);
+
+            switch (command)
+            {
+                case "forward":
+                    position += value;
+                    break;
+                case "down":
+                    depth += value;
+                    break;
+                case "up":
+                    depth -= value;
+                    break;
+            }
+        }
+
+        return new ValueTask<string>((position * depth).ToString());
+    }
+
+    private ValueTask<string> Part2()
+    {
+        var lines = _input.Split("\n").ToArray();
+
+        var position = 0;
+        var depth = 0;
+        var aim = 0;
+
+        foreach (var line in lines)
+        {
+            var splitCommand = line.Split(" ");
+
+            var command = splitCommand[0];
+            var value = int.Parse(splitCommand[1]);
+
+            switch (command)
+            {
+                case "forward":
+                    position += value;
+                    depth += aim * value;
+                    break;
+                case "down":
+                    aim += value;
+                    break;
+                case "up":
+                    aim -= value;
+                    break;
+            }
+        }
+
+        return new ValueTask<string>((position * depth).ToString());
+    }
+}
